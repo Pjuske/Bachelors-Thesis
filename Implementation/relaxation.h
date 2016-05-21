2f771b8e7e4fadc877f5c96cdf33d55c5c57a7f6
Susanne Truong <suzze-t@hotmail.com>
2016-05-21 21:24:33 +0200

    Delete relaxation.h

--- Implementation/relaxation.h
+++
@@ -1,9 +0,0 @@
-#ifndef RELAXATION_H
-#define RELAXATION_H
-
-#include "dijkstra.h"
-
-void initialize_single_source(vertex graph[], vertex source);
-void relax(vertex u, vertex v, int weight);
-void print_path(vertex graph[], vertex source, vertex target);
-#endif
